d1e158257a9dd697ceb0c47d67605fe3c664c088
devblock7 <44695616+devblock7@users.noreply.github.com>
2019-11-11 10:25:05 +0000

    Update server.js

--- server.js
+++ server.js
@@ -4,25 +4,25 @@ var app = express();
 const mongoose = require('mongoose');
 require('dotenv').config();
 
-var port = process.env.PORT || 5000    //Zmkienna do startu serwera
+var port = process.env.PORT || 5000  
 
 app.use(cors());
 app.use(express.json());
 
-const mongoURI = 'mongodb://localhost:27017/diary'    //łączenia bazy tutaj jest nazwa
+const mongoURI = 'mongodb://localhost:27017/diary'  
 
 mongoose.connect(mongoURI,
-  { useNewUrlParser: true, useUnifiedTopology: true  }  //oiekt konfiguracyjny mongoose
+  { useNewUrlParser: true, useUnifiedTopology: true  } 
   )
   .then(() => console.log('MongoDB Connected'))
   .catch(err => console.log(err))
 
 const Users = require('./routes/Users')
-const diarypostRouter = require('./routes/diaryPost')  //tutaj jest Router w nazwie bo zrobiłem to tak jak powinno wyglądac tamtego nie chce już zmieniac
+const diarypostRouter = require('./routes/diaryPost')  
 
 app.use('/users', Users)
 app.use('/diarypost', diarypostRouter)
 
-app.listen(port, function() {                         //Start servera
+app.listen(port, function() {                        
   console.log('Server is running on port: ' + port)
 })
