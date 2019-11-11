77e1085aa6830c6b1dbacb5ae4fa60aa52f9b90c
devblock7 <44695616+devblock7@users.noreply.github.com>
2019-11-11 11:32:39 +0000

    Update Landing.js

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -6,7 +6,7 @@ class Landing extends Component {
       <div className="container">
         <div className="jumbotron mt-5">
           <div className="col-sm-8 mx-auto">
-            <h1 className="text-center">WELCOME AT DIARY</h1>
+            <h1 className="text-center">Welcome in your DiaryApp</h1>
           </div>
         </div>
       </div>
